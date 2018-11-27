a8b00e5c6a55352cd4fa145510583c89ef3a275c
Julia Dang <43449736+jdang95@users.noreply.github.com>
2018-11-26 18:25:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var myImages =[ "http://smartfamilypets.com/wp-content/uploads/2015/12/Chow-chow-puppies-513x330.jpg","http://adogbreeds.com/wp-content/uploads/2012/12/Pembroke-Welsh-Corgi-Puppies-Play-at-Black-Gravel.jpg","https://i.pinimg.com/originals/60/52/e4/6052e467f4d6755cc58321273cbfa5c8.jpg","https://global-free-classified-ads-s02.r.worldssl.net/user_images/1102139.jpg","https://puppyspot-photos-prod.s3.us-west-2.amazonaws.com/breeds/245/card/500000291_medium.jpg"];
+var myImages =[ "https://i.ytimg.com/vi/K5ZSKNccUAw/maxresdefault.jpg","http://adogbreeds.com/wp-content/uploads/2012/12/Pembroke-Welsh-Corgi-Puppies-Play-at-Black-Gravel.jpg","https://i.pinimg.com/originals/60/52/e4/6052e467f4d6755cc58321273cbfa5c8.jpg","https://global-free-classified-ads-s02.r.worldssl.net/user_images/1102139.jpg","https://puppyspot-photos-prod.s3.us-west-2.amazonaws.com/breeds/245/card/500000291_medium.jpg"];
 
 
 var captionImages =["ChowChow","Corgi","BullDog","Yorki","Husky"];
